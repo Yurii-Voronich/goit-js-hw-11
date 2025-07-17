a07b92cf0210673a03d0d86d2b0822388134d5f7
Yurii-Voronich <voronich97@gmail.com>
2025-07-17 15:28:30 +0000

    Deploying to gh-pages from @ Yurii-Voronich/goit-js-hw-11@3fde29064defaab68d46cb2f9831014a863a5649 🚀

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();const i=document.querySelector(".form"),u=n=>{n.preventDefault(),i.elements[0].value};i.addEventListener("submit",u);
+import{a as d,i as a}from"./assets/vendor-BlmI1jqU.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const m=s=>d.get("https://pixabay.com/api/",{params:{key:"51348790-b4b99e7923fa7dbd5fd948fd1",q:s,image_type:"photo",orientation:"horizontal",safesearch:!0}}),f=s=>s.map(({largeImageURL:i,tags:r,webformatURL:l,likes:e,views:t,comments:o,downloads:u})=>`<li class="gallery-item">
+	<a class="gallery-link" href="${i}">
+		<img 
+        width="360"
+		  class="gallery-image" 
+		  src="${l}" 
+		  alt="${r}" 
+		/>
+	</a>
+        <ul class = "describtion">
+        <li class = "describtion-item">Likes <p>${e}</p><li>
+        <li class = "describtion-item">Views <p>${t}</p><li>
+        <li class = "describtion-item">Downloads <p>${u}</p><li>
+        <li class = "describtion-item">Comments <p>${o}</p><li>
+        </ul>
+</li>`).join(""),n=document.querySelector(".form"),c=document.querySelector(".gallery"),p=s=>{let i=null;s.preventDefault(),i=n.elements["search-text"].value.trim(),m(i).then(r=>r.data.hits).then(r=>{r.length===0&&a.error({position:"topRight",title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),c.insertAdjacentHTML("beforeend",f(r))}).catch(r=>{console.log(r),a.error({title:"Error",message:`${r}`})}),n.reset(),c.innerHTML=""};n.addEventListener("submit",p);
 //# sourceMappingURL=index.js.map
