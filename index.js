ca3be9b821276ff96f7e4b167367a60aeec25336
Yurii-Voronich <voronich97@gmail.com>
2025-07-19 07:20:19 +0000

    Deploying to gh-pages from @ Yurii-Voronich/goit-js-hw-11@ee977ea23a0c59f0d630b4ec7c1840017b2f0321 🚀

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import{a as d,i as a}from"./assets/vendor-CYk3T505.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const m=s=>d.get("https://pixabay.com/api/",{params:{key:"51348790-b4b99e7923fa7dbd5fd948fd1",q:s,image_type:"photo",orientation:"horizontal",safesearch:!0}}),f=s=>s.map(({largeImageURL:i,tags:r,webformatURL:n,likes:e,views:t,comments:o,downloads:u})=>`<li class="gallery-item">
-	<a class="gallery-link" href="${i}">
+import{a as m,S as f,i as a}from"./assets/vendor-Dx9D5Pvt.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&l(i)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function l(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const p=o=>m.get("https://pixabay.com/api/",{params:{key:"51348790-b4b99e7923fa7dbd5fd948fd1",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}}),g=o=>o.map(({largeImageURL:s,tags:t,webformatURL:l,likes:e,views:r,comments:i,downloads:d})=>`<li class="gallery-item">
+	<a class="gallery-link" href="${s}">
 		<img 
         width="360"
 		  class="gallery-image" 
-		  src="${n}" 
-		  alt="${r}" 
+		  src="${l}" 
+		  alt="${t}" 
 		/>
 	</a>
         <ul class = "describtion">
         <li class = "describtion-item">Likes <p>${e}</p><li>
-        <li class = "describtion-item">Views <p>${t}</p><li>
-        <li class = "describtion-item">Downloads <p>${u}</p><li>
-        <li class = "describtion-item">Comments <p>${o}</p><li>
+        <li class = "describtion-item">Views <p>${r}</p><li>
+        <li class = "describtion-item">Downloads <p>${d}</p><li>
+        <li class = "describtion-item">Comments <p>${i}</p><li>
         </ul>
-</li>`).join(""),l=document.querySelector(".form"),c=document.querySelector(".gallery"),p=s=>{let i=null;s.preventDefault(),i=l.elements["search-text"].value.trim(),m(i).then(r=>r.data.hits).then(r=>(r.length===0&&a.error({position:"topRight",title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),c.insertAdjacentHTML("beforeend",f(r)))).catch(r=>{console.log(r),a.error({title:"Error",message:`${r}`})}),l.reset(),c.innerHTML=""};l.addEventListener("submit",p);
+</li>`).join(""),y=()=>{c.innerHTML=""},h=new f(".gallery a");function b(){h.refresh()}const L=()=>{u.classList.remove("visually-hidden")},q=()=>{u.classList.add("visually-hidden")},n=document.querySelector(".form"),c=document.querySelector(".gallery"),u=document.querySelector(".loader"),v=o=>{o.preventDefault(),y();const s=n.elements["search-text"].value.trim();if(!s)return console.log("pustrequset"),a.error({position:"topRight",title:"Error",message:"please fill your search request"});L(),p(s).then(t=>t.data.hits).then(t=>{t.length===0&&a.error({position:"topRight",title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),c.insertAdjacentHTML("beforeend",g(t)),b()}).catch(t=>{console.log(t),a.error({title:"Error",message:`${t}`})}).finally(t=>{q()}),n.reset()};n.addEventListener("submit",v);
 //# sourceMappingURL=index.js.map
